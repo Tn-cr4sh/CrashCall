9adb9dd2f3ae6648122c99035d08a38c2fd94a93
Tn-cr4sh <48906852+Tn-cr4sh@users.noreply.github.com>
2019-05-11 00:49:51 +0700

    Update CrashCall.py

--- CrashCall.py
+++ CrashCall.py
@@ -2,40 +2,12 @@ try:
 	import mechanize,os,sys,time
 	
 	print("""\033[97m
-                                                                       
-                             .--:////::-.                             
-                       -/ooo+/:--....--:/+ooo/-                       
-                   .+so/.                    ./oo+-                   
-                .+s+.                            ./so.                
-              -so.                                  .+s:              
-            -y+`                                      `/y-            
-          `ss`   `/+.      `..--.:--:.--..`      .+/`   `os`          
-         -h: `:-smo`   `...`..` -`..`- `..`...`    omy-:` -h-         
-        :h. :d-dy+.  `-....:`  .. .. .. `.:-...-`  `+yd-d: .h:        
-       -h`.-my:oy+ `-.   ..``.-:-yooho:.`` ..   .-` +yo:sm--`h-       
-      `d..d-msds. `-    ..    -` :-+m+`-    ..    -. .sdsm-d..d`      
-      s+ sm-d+++ .-...``-     -   .+   -`    -```..-. +++d-my +s      
-     .d``ym:sms `-    `-.....--...os...--.....:``   -` sms:my``d.     
-     +s o:myd/. -`     :     -`   ::`  `-     :     `- ./dym:o s+     
-     s/`m-sh.y. :     `-     -`.-oyyo-.`-     -`     : .y-ys-m`/s     
-     y/ dd::dh  :......:...-:oym``hh` dyo:-...-......:` hd::dd /y     
-     s/ /mhsm:. :     `-odmmmmms `+o` smmmmmdo-`     : .:mshm/ /s     
-     +s`s:dho:s -`     /mmmmmmmy  dd  ymmmmmmm/     `- s:ohd:s`s+     
-     .d`oh:o/sm``-   ``smmmmmmmm/ mm /mmmmmmmms.`   -` ms/o:ho`d.     
-      s+ omh-ym./.-..``ymmmmmmmmmommommmmmmmmmy `..-./.my-hmo +s      
-      `d.`/smym:ss.-  `mmmmmmmmmmmmmmmmmmmmmmmm`  -.ss:myms/`.d`      
-       -h`/s::sy:mo`-.:mmmmmmmmmmmmmmmmmmmmmmmm:.-`om/ys/:s/`h-       
-        :h.-ydyo:sm/o/+mmmmmmmmmmmmmmmmmmmmmmmm+/o/ms:oydy-.h:        
-         .h: .oyddhd:hdmmmmmmmmmmmmmmmmmmmmmmmmdh:dhddyo. :h-         
-          `os`.++/+os/ohdmmmmmmmmmmmmmmmmmmmmdho:so+/++.`ss`          
-            -y+``/oyhhhyo+odmmmmmmmmmmmmmmmo+oyhhhyo/.`+y-            
-              -so.`/ssyhdMMmmmmmmmmmmmmmmmmMMdhyss/`.os-              
-                .+s+.`-::--mmmmmmmmmmmmmmmm--::-`.+s+.                
-                   .+oo/. `mmmmmmmmmmmmmmmm. ./os+.                   
-                       ./osdmmmmmmmmmmmmmmdso/-                       
-                             `--::///::-.                               
+ _   _  _____  _____  _____    _____  ____  ____ 
+( )_( )(  _  )(  _  )(  _  )  (  _  )(_  _)(  _ \\
+ ) _ (  )(_)(  )(_)(  )(_)(    )(_)(   )(   )___/
+(_) (_)(_____)(_____)(___/\\\  (_____) (__) (__)  
 """)
-
+	br = mechanize.Browser()
 	br.set_handle_equiv(True)
 	br.set_handle_gzip(True)
 	br.set_handle_redirect(True)
